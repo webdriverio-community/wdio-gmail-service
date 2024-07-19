be79e4e052f7177f3ce3960d5e0d7fe054c0a0e9
Christian Bromann <git@bromann.dev>
2024-07-18 18:19:54 -0700

    chore: update to WebdriverIO v9

---
+++ vitest.config.ts
@@ -0,0 +1,33 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    test: {
+        include: ['tests/**/*.test.ts'],
+        /**
+         * not to ESM ported packages
+         */
+        exclude: [
+            'dist', '.idea', '.git', '.cache',
+            '**/node_modules/**'
+        ],
+        coverage: {
+            enabled: true,
+            provider: 'v8',
+            exclude: [
+                'build',
+                '__fixtures__',
+                '**/*.test.ts',
+                'vitest.config.ts',
+                '.eslintrc.cjs',
+                'src/cjs'
+            ],
+            thresholds: {
+                lines: 100,
+                functions: 100,
+                branches: 100,
+                statements: 100
+            }
+        }
+    }
+})
